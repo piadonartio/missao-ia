9c4a9dcaba994f9f607ad34075ed41a675f4a33d
rodrigo <140639687+piadonartio@users.noreply.github.com>
2025-10-22 08:12:07 -0300

    00

---
+++ java/script.js
@@ -0,0 +1,14 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultados = document.querySelector(".caixa-resultados");
+const textoResultado = document.querySelector(".texto-resultados");
+const botaoJogarNovamente = document.querySelector(".novamente-botao");
+const telaInicial = document.querySelector(".tela-inicial");
+const botaoIniciar = document.querySelector(".iniciar-botao");
+
+let atual = 0;
+let PerguntaAtual;
+let historiaFinal = "";
+
+botaoIniciar.addEventListener('click,iniciaJogo');
