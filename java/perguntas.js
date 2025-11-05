d1e2b5f1eee536a52510992bef0e4a51fa53bd5a
rodrigo <140639687+piadonartio@users.noreply.github.com>
2025-11-05 14:14:16 -0300

    00

---
+++ java/perguntas.js
@@ -0,0 +1,13 @@
+export const perguntas = [
+    {
+    enunciado:"Maria e João são pais de Inês, uma menina de 5 anos curiosa e cheia de energia. Recentemente, Inês tem demonstrado um interesse crescente pelos jogos educativos e vídeos infantis no tablet que a avó lhe ofereceu. Embora reconheçam o potencial de aprendizagem e entretenimento, Maria e João observam que o tempo de tela de Inês tem aumentado, e ela demonstra frustração quando o dispositivo é retirado. A questão central que os pais enfrentam é: Como estabelecer um uso de telas que maximize os benefícios (como a alfabetização digital e o conteúdo educativo) sem comprometer o seu desenvolvimento social, físico e emocional? O que você faria?",
+    alternativas:[
+        {
+            texto: "Maria e João decidem integrar ativamente a tecnologia na rotina de Inês, mas com regras estritas e participação adulta.",
+            afirmacao:[
+                "A adoção da Alternativa A (Uso Guiado) é a mais estratégica e responsável, pois reconhece que a tecnologia é uma ferramenta inevitável e essencial no século XXI, e que o papel dos pais não é o de proibir, mas sim o de alfabetizar digitalmente Inês de forma segura e intencional."
+            ],
+        }
+    ]
+}
+]
--- java/script.js
+++ java/script.js
@@ -12,3 +12,67 @@ let PerguntaAtual;
 let historiaFinal = "";
 
 botaoIniciar.addEventListener('click,iniciaJogo');
+function iniciaJogo (){
+    atual = 0;
+    historiaFinal = "";
+    telaInicial.style.display = 'none';
+    caixaPerguntas.classList.remove("mostrar");
+    caixaAlternativas.classList.remove("mostrar");
+    caixaResultados.classList.remove("mostrar");
+    mostraPergunta();
+}
+
+function mostraPergunta (){
+    if (atual >= perguntas.length){
+        mostraResultado();
+        return;
+    }
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+    for (const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+    function mostraAfirmacoes(){
+        for(const afirmacoes of perguntaAtual.afirmacoes){
+
+        }
+    }
+}
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacoes = aleatorio(opcaoSelecionada.afirmacao);
+    historiaFinal += afirmacoes + "";
+    if (opcaoSelecionada.proxima !== undefined){
+        atual = opcaoSelecionada.proxima;
+    }else{
+        mostraResultado();
+        return;
+    }
+    mostraPergunta();
+}
+function mostraResultado(){
+    caixaPerguntas.textContent = 'Em 2050, ${nome}';
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+    caixaResultados.classList.add("mostrar");
+    botaoJogarNovamente.addEventListener("click", jogaNovamente);
+}
+function jogaNovamente(){
+    atual = 0;
+    historiaFinal = "";
+    caixaResultados.classList.remove("mostrar");
+    mostraPergunta();
+}
+function substituiNome(){
+    for (const pergunta of perguntas){
+        pergunta.enunciado = pergunta.enunciado.replace(/você/g, nome);
+    }
+}
+substituiNome();
