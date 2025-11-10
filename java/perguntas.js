97e2a9d9d2bad17235775e91a44c01f0d1bb138f
rodrigo <140639687+piadonartio@users.noreply.github.com>
2025-11-10 09:27:39 -0300

    00

--- java/perguntas.js
+++ java/perguntas.js
@@ -3,35 +3,35 @@ export const perguntas = [
     enunciado:"A inserção de dispositivos digitais e telas (como smartphones, tablets e televisões) na rotina de crianças em idade pré-escolar e escolar é um fenômeno global que suscita preocupações e oportunidades. Considerando o desenvolvimento infantil integral, qual dos dilemas a seguir melhor representa os impactos divergentes do uso de telas?",
     alternativas:[
         {
-            texto: "O uso moderado e supervisionado de telas serve como um poderoso instrumento de enriquecimento pedagógico.",
+            texto: "A transição para a mobilidade elétrica representa um imperativo ecológico e um salto de qualidade de vida urbana.",
             afirmacao:[
-                "Telas, quando usadas de forma moderada e mediada, são catalisadores educacionais. Elas oferecem ferramentas interativas que promovem o raciocínio lógico, estimulam a criatividade e desenvolvem a fluência tecnológica, preparando a criança ativamente para o futuro."
+                "Os veículos elétricos eliminam integralmente as emissões locais de poluentes, mitigando a névoa tóxica das cidades e o impacto direto na saúde respiratória. Em paralelo, e considerando o ciclo de vida completo, promovem uma redução drástica na pegada de carbono global, especialmente quando abastecidos por matrizes energéticas limpas, consolidando-se como a espinha dorsal de um futuro sustentável.."
             ],
             proxima: 1,
         },
         {
-            texto:"A exposição excessiva e passiva a telas, especialmente sem conteúdo educativo, está associada à redução drástica do tempo dedicado a brincadeiras não estruturadas e atividades físicas.",
+            texto:"Dirigir um veículo elétrico é experimentar o ápice da performance automotiva moderna, combinando potência instantânea com serenidade.",
             afirmacao:[
-                "A exposição excessiva e passiva está associada à perda de tempo de atividades cruciais (brincadeiras e exercícios). Isso pode levar ao sedentarismo, atraso na linguagem e dificuldades na regulação emocional, comprometendo o desenvolvimento físico e mental."
+                "Graças ao torque máximo disponível a 0 RPM, os VEs oferecem uma aceleração fluida, imediata e silenciosa, elevando o prazer da condução a um novo patamar. O baixo centro de gravidade, proporcionado pelo posicionamento da bateria, ainda garante estabilidade superior e manuseio preciso, conferindo uma sensação de controle inigualável.."
             ],
             proxima: 2,
         }
     ]
 },
 {
-    enunciado:"A mediação parental no uso de telas é amplamente considerada um fator crucial para maximizar os benefícios e minimizar os riscos. Essa mediação pode se manifestar de diversas formas: desde a Mediação Restritiva até a Mediação Ativa/Co-uso. Qual das seguintes afirmações melhor descreve o dilema central entre a eficácia e o impacto psicológico dos diferentes tipos de mediação parental?",
+    enunciado:"A capacidade de abastecer o veículo no conforto e segurança do lar é a maior conveniência da era elétrica.",
     alternativas:[
         {
-            texto:"A Mediação Ativa (Co-uso) é superior à Restritiva, pois transforma o tempo de tela em uma oportunidade de vínculo e aprendizado mútuo.",
+            texto:"Carregar o VE durante a noite, com a tarifa de energia mais econômica, transforma o ato de recarga em um hábito rotineiro e de baixo custo, eliminando a dependência das bombas de combustível. Essa integração da recarga à rotina doméstica ou profissional maximiza a autonomia diária e otimiza radicalmente os gastos com energia..",
             afirmacao:[
-                  "O Co-uso transforma o tempo de tela em uma oportunidade de vínculo, diálogo e aprendizado mútuo. Ao interagir com o conteúdo junto, os pais ensinam a criança a ser um consumidor digital crítico e discernente, promovendo a competência digital e a autorregulação de forma saudável."
+                  "Graças à sua arquitetura avançada, os VEs permitem atualizações de software Over-The-Air (OTA) que aprimoram o desempenho, a segurança e adicionam novos recursos."
             ],
             proxima:3,
         },
         {
-            texto:"A Mediação Estritamente Restritiva, embora eficaz para limitar o tempo de tela, pode ser contraproducente a longo prazo.",
+            texto:"O ciclo de vida das baterias de íon-lítio é um exemplo de economia circular e uso inteligente de recursos.",
             afirmacao: [
-                  "O controle excessivo baseado apenas na proibição mina a autonomia e impede o desenvolvimento da autorregulação digital interna. Isso pode levar a um aumento da curiosidade pelo conteúdo proibido, podendo resultar em uso escondido ou problemático na adolescência, em vez de preparar o jovem para escolhas responsáveis."
+                  "Longe de serem um passivo ambiental, as baterias automotivas esgotadas em seu uso primário são recondicionadas para uso secundário em sistemas estacionários de armazenamento de energia (residencial ou de rede elétrica)."
             ],
             proxima: 4,
         }
